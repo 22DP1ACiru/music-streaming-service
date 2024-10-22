1906d89a6576252ef3eb42632f5e8beb790da7c9
Aigars Cīrulis <11dpacirulis@rvt.lv>
2024-10-22 11:59:46 +0300

    Backend - add model content, API routes

--- backend/app/Models/Song.php
+++ backend/app/Models/Song.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'genre',
+        'song_file',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function albums()
+    {
+        return $this->belongsToMany(Album::class, 'album_song');
+    }
+
+    public function playlists()
+    {
+        return $this->belongsToMany(Playlist::class, 'playlist_song');
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::resource('users', UserController::class);
+route::resource('albums', AlbumController::class);
+route::resource('songs', SongController::class);
+route::resource('playlists', PlaylistController::class);
 
