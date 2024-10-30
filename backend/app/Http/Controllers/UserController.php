b7f074e735ec57e54891f5f214fac0e2d2038376
Aigars Cīrulis <11dpacirulis@rvt.lv>
2024-10-30 13:56:33 +0200

    Backend - small change in UserController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
